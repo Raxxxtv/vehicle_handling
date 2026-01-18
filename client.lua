f3bec2fca259522562b037f083cce47a4ce9506d
Raxxxtv <raxtv.mail@gmail.com>
2026-01-18 16:03:14 +0100

    Combining Functions

--- client.lua
+++ client.lua
@@ -1,8 +1,6 @@
 local ESX = exports['es_extended']:getSharedObject()
 
 local speedChanged = false
-local monitoringExit = false
-local leftVehicleThread = false
 
 -- Capping the possible Numbers
 local function clamp(value, min, max)
@@ -12,136 +10,77 @@ end
 local speedThreadId = 0 -- Sets ThreadId
 
 local function changeVehicleSpeed(vehicle, multiplier)
-    if speedChanged then return end
-    speedChanged = true
-    -- Checks if the vehicle exists
-    if not DoesEntityExist(vehicle) then 
-        speedChanged = false
-        return 
-    end
-    speedThreadId = speedThreadId + 1
-    local myThreadId = speedThreadId -- Changes the current ThreadId to the global ThreadId
+    if speedChanged then 
+        if multiplier == 0 then
+            speedThreadId = speedThreadId + 1
 
-    local powerValue = clamp(multiplier * Config.PowerValueScale, 1.0, 1.8) -- Sets value to 1 if the value is under one and to 1.8 if the value is over 1.8, if its in the range, then the value stays the same
-
-    CreateThread(function()
-        while speedThreadId == myThreadId do -- Runs the Code while speedThreadId has the same value as myThreadId
-            SetVehicleCheatPowerIncrease(vehicle, powerValue) -- Changes the handling of the vehicle
-            Wait(0)
-        end
-        speedChanged = false
-    end)
-end
-
--- Resets the handling of the vehicle
-local function resetVehicle(vehicle)
-    if not DoesEntityExist(vehicle) then
-        speedChanged = false
-        monitoringExit = false
-        ESX.ShowNotification('Fahrzeugbeschleunigung zurückgesetzt', 'success', 5000, 'Handlingystem')
-        return
-    end
-
-    SetVehicleCheatPowerIncrease(vehicle, 1)
-    speedThreadId = speedThreadId + 1
-
-    monitoringExit = false
-
-    if Config.EnableMaxSpeedLimit and speedChanged then
-        ModifyVehicleTopSpeed(vehicle, 1) -- Resets the vehicle max speed
-    end
-    speedChanged = false
-
-    ESX.ShowNotification('Fahrzeugbeschleunigung zurückgesetzt', 'success', 5000, 'Handlingystem')
-end
--- Checks if the player left the vehicle
-local function leftVehicle()
-    if leftVehicleThread then return end
-    leftVehicleThread = true
-    CreateThread(function()
-        local wasInVehicle = false
-        local wasDriver = false
-        local lastVehicle = nil
-
-        while monitoringExit do
-            local ped = ESX.PlayerData.ped
-            local isInVehicle = IsPedInAnyVehicle(ped)
-
-            if isInVehicle then
-                local vehicle = GetVehiclePedIsIn(ped)
-                lastVehicle = vehicle
-                wasDriver = (GetPedInVehicleSeat(vehicle, -1) == ped)
-                wasInVehicle = true
-            else
-                if wasInVehicle and wasDriver then
-                    resetVehicle(lastVehicle)
-                end
-
-                wasInVehicle = false
-                wasDriver = false
-                lastVehicle = nil
+            if Config.EnableMaxSpeedLimit then
+                ModifyVehicleTopSpeed(vehicle, 1) -- Resets the vehicle max speed
             end
-            Wait(200)
+            speedChanged = false
+
+            ESX.ShowNotification('Fahrzeugbeschleunigung zurückgesetzt', 'success', 5000, 'Handlingystem')
+        else
+            ESX.ShowNotification('Das Fahrzeug ist bereits Modifiziert', 'success', 5000, 'Handlingystem')
         end
-        leftVehicleThread = false
-    end)
-end
-
-RegisterNetEvent('vehicleSpeed:applyMultiplier', function(multiplier)
-
-    -- Checks if the player is logged in his char
-    if not ESX.PlayerLoaded then return end
-    
-
-    local playerPed = ESX.PlayerData.ped
-    -- Checks if the player is in a Vehicle 
-    if not IsPedInAnyVehicle(playerPed) then
-        ESX.ShowNotification('Du befindest dich in keinem Fahrzeug', 'error', 5000, 'Handlingystem')
         return
     end
-    -- Checks if the handling was already changed and if multiplier equals 0. If the handling wasnt changed and the multiplier equals 0 then the player becomes an error.
-    if not speedChanged and multiplier == 0 then
-        ESX.ShowNotification('Dein Fahrzeug wurde nicht verändert', 'error', 5000, 'Handlingystem')
+    speedChanged = true
+    local myThreadId = speedThreadId -- Changes the current ThreadId to the global ThreadId
+    local mult = multiplier * 1.0
+    -- Checks if the vehicle exists
+    if DoesEntityExist(vehicle) == 0 then
+        speedChanged = false
         return
     end
-
-    multiplier = clamp(multiplier, 0, Config.MaxMultiplier)
-
-    local vehicle = GetVehiclePedIsIn(playerPed, false)
-
-    if not DoesEntityExist(vehicle) then return end
-    -- Checks if the player is on the Driver Seat
-    if ESX.PlayerData.ped ~= GetPedInVehicleSeat(vehicle, -1) then 
-        ESX.ShowNotification('Du befindest dich nicht auf dem Fahrersitz', 'error', 5000, 'Handlingystem')
-        return 
-    end
-
-    -- multiplier 0 = reset
-    if multiplier == 0 then
-        resetVehicle(vehicle)
-        return
-    end
-    changeVehicleSpeed(vehicle, multiplier)
-    monitoringExit = true
-    leftVehicle()
 
     -- changes the MaxSpeed
     if Config.EnableMaxSpeedLimit then
         local maxSpeedMultiplier = multiplier * 0.04 + 1.0
         ModifyVehicleTopSpeed(vehicle, maxSpeedMultiplier) -- Multiplies the vehicle max speed
     end
-
     ESX.ShowNotification(('Fahrzeugbeschleunigung gesetzt: %sx'):format(multiplier), 'success', 5000, 'Handlingystem')
+
+    CreateThread(function()
+        while speedThreadId == myThreadId do -- Runs the Code while speedThreadId has the same value as myThreadId
+            print(speedThreadId, myThreadId)
+            SetVehicleCheatPowerIncrease(vehicle, mult) -- Changes the handling of the vehicle
+            Wait(0)
+        end
+        speedChanged = false
+    end)
+end
+
+
+RegisterNetEvent('vehicleSpeed:applyMultiplier', function(multiplier)
+
+    -- Checks if the player is logged in his char
+    if not ESX.PlayerLoaded then return end
+    local playerPed = ESX.PlayerData.ped
+
+    local vehicle = GetVehiclePedIsIn(playerPed, false)
+
+    if DoesEntityExist(vehicle) == 0 then -- Checks if the player is in a Vehicle 
+        ESX.ShowNotification('Du befindest dich in keinem Fahrzeug', 'error', 5000, 'Handlingystem')
+        return
+    end
+
+    multiplier = clamp(multiplier, 0, Config.MaxMultiplier)
+    -- Checks if the player is on the Driver Seat
+    if ESX.PlayerData.ped ~= GetPedInVehicleSeat(vehicle, -1) then 
+        ESX.ShowNotification('Du befindest dich nicht auf dem Fahrersitz', 'error', 5000, 'Handlingystem')
+        return 
+    end
+
+    if multiplier == 0 and not speedChanged then -- Checks if the handling was already changed and if multiplier equals 0. If the handling wasnt changed and the multiplier equals 0 then the player becomes an error.
+        ESX.ShowNotification('Dein Fahrzeug wurde nicht verändert', 'error', 5000, 'Handlingystem')
+        return
+    end
+
+    changeVehicleSpeed(vehicle, multiplier)
 end)
 
--- Gets Triggered if the player dies
-AddEventHandler('esx:onPlayerDeath', function()
-    local ped = ESX.PlayerData.ped
-    if IsPedInAnyVehicle(ped) then
-        local vehicle = GetVehiclePedIsIn(ped)
-        local isDriver = (GetPedInVehicleSeat(vehicle, -1) == ped)
-        if speedChanged and isDriver then
-            resetVehicle(GetPlayersLastVehicle())
-        end
-    end
+AddEventHandler('esx:enteredVehicle', function(vehicle, plate, seat, displayName, netId)
+    speedChanged = false
+    speedThreadId = speedThreadId + 1
 end)
